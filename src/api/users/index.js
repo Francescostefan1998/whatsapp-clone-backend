ca69ca0c8c8ca6b691d8455c91bacaded26fc995
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-02-24 15:36:45 +0100

    creating the users main endpoint
    creating the users main endpoint

---
+++ src/api/users/index.js
@@ -0,0 +1,79 @@
+import express from "express";
+import createHttpError from "http-errors";
+import passport from "passport";
+import UsersModel from "./model.js";
+const userRouter = express.Router();
+
+userRouter.get("/", async (req, res, next) => {
+  try {
+    const users = await UsersModel.find();
+    res.send(users);
+  } catch (error) {
+    next(error);
+  }
+});
+
+userRouter.post("/", async (req, res, next) => {
+  try {
+    console.log("POST");
+    const newUser = new UsersModel(req.body);
+    const { _id } = await newUser.save();
+    res.status(201).send({ _id });
+  } catch (error) {
+    next(error);
+  }
+});
+userRouter.get("/:userId", async (req, res, next) => {
+  try {
+    const user = await UsersModel.findById(req.params.userId);
+    if (user) {
+      res.send(user);
+    } else {
+      next(
+        createHttpError(404, `BlogPost wit id ${req.params.userId} not found`)
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+userRouter.put("/:userId", async (req, res, next) => {
+  try {
+    const updateUser = await UsersModel.findByIdAndUpdate(
+      req.params.userId,
+      req.body,
+      { new: true, runValidators: true }
+    );
+    if (updateUser) {
+      res.send(updateUser);
+    } else {
+      next(
+        createHttpError(404, `BlogPost wit id ${req.params.userId} not found`)
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+userRouter.delete(
+  "/:userId",
+
+  async (req, res, next) => {
+    try {
+      const deleteUser = await UsersModel.findByIdAndDelete(req.params.userId);
+      if (deleteUser) {
+        res.status(204).send("deleted");
+      } else {
+        next(
+          createHttpError(404, `BlogPost wit id ${req.params.userId} not found`)
+        );
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+export default userRouter;
--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ import listEndpoints from "express-list-endpoints";
 import cors from "cors";
 import mongoose from "mongoose";
 import passport from "passport";
+import userRouter from "./api/users/index.js";
 const server = express();
 const port = process.env.PORT || 3001;
 
 server.use(cors());
 server.use(express.json());
+server.use("/users", userRouter);
 mongoose.connect(process.env.MONGO_URL);
 mongoose.connection.on("connected", () => {
   console.log("Connected to mongo db");
