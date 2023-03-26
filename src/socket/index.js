decf2ccfca510cf05ea8f64df7b0d41952f7470d
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-03-26 20:21:26 +0200

    update socketio

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,6 +1,21 @@
 import express from "express";
-
+let onlineUsers = [];
 export const newConnectionHandler = (newClient) => {
   console.log("new connection", newClient.id);
   newClient.emit("welcome", { message: `Hello ${newClient.id}` });
+  newClient.on("setUsername", (payload) => {
+    console.log(payload);
+    onlineUsers.push({ username: payload.firstName, socketId: newClient.id });
+    newClient.emit("loggedIn", onlineUsers);
+    newClient.broadcast.emit("updateOnlineUsers", onlineUsers);
+  });
+  newClient.on("sendMessage", (message) => {
+    console.log("new message", message);
+    newClient.broadcast.emit("newMessage", message);
+  });
+
+  newClient.on("disconnect", () => {
+    onlineUsers = onlineUsers.filter((user) => user.socketId !== newClient.id);
+    newClient.broadcast.emit("updateOnlineUsers", onlineUsers);
+  });
 };
